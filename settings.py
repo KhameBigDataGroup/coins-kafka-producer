ff1ef834ffd8055bb8f66b0647abc91e31ea2452
seyed <moosavia2@gmail.com>
2019-08-13 23:59:10 +0430

    Add logging config

--- settings.py
+++ settings.py
@@ -1,4 +1,41 @@
 import os
+from logging.config import dictConfig
+
+dictConfig({
+    "version": 1,
+    "formatters": {
+      "simple": {
+        "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+      },
+      "verbose": {
+        "format": "%(levelname)3.3s %(asctime)22.22s %(process)7d [%(name)s:%(funcName)s] %(message)s"
+      }
+    },
+    "handlers": {
+      "null": {
+        "level": "DEBUG",
+        "class": "logging.NullHandler"
+      },
+      "console": {
+        "level": "DEBUG",
+        "class": "logging.StreamHandler"
+      },
+      "file": {
+        "level": "DEBUG",
+        "class": "logging.FileHandler",
+        "filename": "/logs/debug.log",
+        "formatter": "simple"
+      }
+    },
+    "loggers": {
+      "root": {
+        "handlers": [
+          "console",
+        ],
+        "level": "INFO"
+      }
+    }
+})
 
 conf = {'bootstrap.servers': os.environ.get('BOOSTRAP_SERVER'),
         'queue.buffering.max.ms': 400,
